ebc1e27da2b2098c943a46b774f20fe1474fdf16
strevizo <deri411@msn.com>
2020-04-05 15:06:38 -0500

    Student app

---
+++ src/Student.java
@@ -0,0 +1,33 @@
+
+public class Student{
+	private String name;
+	private String ssn;
+	private String userID;
+	private String email;
+	private static final int id = 1231;
+	
+	Student(String name, String ssn){
+		this.name = name;
+		this.ssn = ssn;
+		email = name.replaceAll(" ",".") + "@newbieuni.edu";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	
+}
+
---
+++ src/StudentApp.java
@@ -0,0 +1,8 @@
+
+public class StudentApp {
+	public static void main(String[] args) {
+		Student stu1 = new Student("Pedro Paramo","254465487");
+		System.out.println(stu1.getEmail());
+	}
+}
+
